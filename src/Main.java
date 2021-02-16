dc79db474c9a3627190448b0cd7edd7a2658baf0
r00t_k1n9 <46450756+soryecker@users.noreply.github.com>
2021-02-16 08:36:04 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,83 @@
-				
+import java.io.*;
 
-			}
+import java.net.*;
 
-			catch (Exception e)
+import java.util.Scanner;
 
-			{
+public class Main
 
-				System.out.println(e);
+{
 
-			}
+    
+
+	public static void main(String[] args) throws IOException	{
+
+        File file = new File("/sdcard/图片.txt");
+
+        Writer wr = new FileWriter(file);
+
+        
+
+		int year = 0;//年份最后以后数字
+
+        //Main ma = new Main();
+
+		for (int b = 1;b <= 366;b++)
+
+        {//日期范围
+
+			if (("" + b).length() == 1)
+
+            {
+
+				String twozero = "00" + b;
+
+                
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "/" + "2" + year + twozero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-2/" + "2" + year + twozero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-3/" + "2" + year + twozero  +".jpg\r\n");
+
+            }
+
+            else if (("" + b).length() == 2)
+
+            {
+
+                String onezero = "0" + b;
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "/" + "2" + year + onezero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-2/" + "2" + year + onezero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-3/" + "2" + year + onezero  +".jpg\r\n");
+
+            }
+
+            else if (("" + b).length() == 3)
+
+            {
+
+                String onezero = "" + b;
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "/" + "2" + year + onezero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-2/" + "2" + year + onezero  +".jpg\r\n");
+
+                wr.write("https://madamevoyeur.com/images/202" + year + "-3/" + "2" + year + onezero  +".jpg\r\n");
+
+                
+
+            }
 
 		}
 
-		
+        System.out.println("[*]完成！");
 
-		}
+        wr.close();
 
-		
+    }
 
-		
-
-		}
-
-	
-
-	public static void download(String urlString, String filename,String savePath,long restTime) throws Exception { 
-
-		Thread.sleep(restTime);
-
-        // 构造URL 
-
-        URL url = new URL(urlString); 
-
-		System.out.println(urlString);
-
-        // 打开连接 
-
-        URLConnection con = url.openConnection(); 
-
-        //设置请求超时为5s 
-
-        con.setConnectTimeout(5*1000); 
-
-        //防止简单反扒
-
-        String referer = url.getProtocol()+"://"+url.getHost();
-
-        con.setRequestProperty("Referer", referer);
-
-        // 输入流 
-
-        InputStream is = con.getInputStream(); 
-
-        // 1K的数据缓冲 
-
-        byte[] bs = new byte[1024]; 
+}
